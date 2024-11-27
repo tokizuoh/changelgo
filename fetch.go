3bce3d3b48e0358ae53058095a26d00db349bec7
tokizuoh <tokizuoh@gmail.com>
2024-11-28 07:45:06 +0900

    refactor: separate fetch from main

---
+++ fetch.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func fetch() (Response, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return Response{}, fmt.Errorf("GITHUB_TOKEN is not set")
+	}
+
+	queryFile := "Query.graphql"
+	queryBytes, err := os.ReadFile(queryFile)
+	if err != nil {
+		return Response{}, fmt.Errorf("Failed to read query file: %v\n", err)
+	}
+	query := string(queryBytes)
+
+	// TODO: DI
+	variables := map[string]interface{}{
+		"owner":    "swiftlang",
+		"name":     "swift",
+		"first":    5,
+		"filePath": "CHANGELOG.md",
+	}
+
+	body := map[string]interface{}{
+		"query":     query,
+		"variables": variables,
+	}
+
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return Response{}, fmt.Errorf("Failed to marshal body: %v\n", err)
+	}
+
+	req, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		return Response{}, fmt.Errorf("Failed to create request: %v\n", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return Response{}, fmt.Errorf("Failed to send request: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Response{}, fmt.Errorf("Failed to read response: %v\n", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return Response{}, fmt.Errorf("Non-OK HTTP status: %d\nResponse: %s\n", resp.StatusCode, string(respBody))
+	}
+
+	var response Response
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return Response{}, fmt.Errorf("Failed to parse response: %v\n", err)
+	}
+
+	return response, nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"regexp"
 	"time"
@@ -15,72 +11,10 @@ import (
 )
 
 func main() {
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		fmt.Println("GITHUB_TOKEN is not set")
-		return
-	}
-
-	queryFile := "Query.graphql"
-	queryBytes, err := os.ReadFile(queryFile)
+	response, err := fetch()
 	if err != nil {
-		fmt.Printf("Failed to read query file: %v\n", err)
-		return
-	}
-	query := string(queryBytes)
-
-	// TODO: DI
-	variables := map[string]interface{}{
-		"owner":    "swiftlang",
-		"name":     "swift",
-		"first":    5,
-		"filePath": "CHANGELOG.md",
-	}
-
-	body := map[string]interface{}{
-		"query":     query,
-		"variables": variables,
-	}
-
-	bodyBytes, err := json.Marshal(body)
-	if err != nil {
-		fmt.Printf("Failed to marshal body: %v\n", err)
-		return
-	}
-
-	req, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewBuffer(bodyBytes))
-	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		return
-	}
-
-	req.Header.Set("Authorization", "Bearer "+token)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Failed to send request: %v\n", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Failed to read response: %v\n", err)
-		return
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Non-OK HTTP status: %d\nResponse: %s\n", resp.StatusCode, string(respBody))
-		return
-	}
-
-	var response Response
-	err = json.Unmarshal(respBody, &response)
-	if err != nil {
-		fmt.Printf("Failed to parse response: %v\n", err)
-		return
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	feed := &feeds.Feed{
