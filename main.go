5746f51357b70b55ef0439bfd59c5e6692abdd93
tokizuoh <tokizuoh@gmail.com>
2024-11-27 16:34:21 +0900

    feat: read query file from local

--- main.go
+++ main.go
@@ -11,4 +11,12 @@ func main() {
 		fmt.Println("GITHUB_TOKEN is not set")
 		return
 	}
+
+	queryFile := "Query.graphql"
+	queryBytes, err := os.ReadFile(queryFile)
+	if err != nil {
+		fmt.Printf("Failed to read query file: %v\n", err)
+		return
+	}
+	query := string(queryBytes)
 }
