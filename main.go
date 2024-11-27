1306be3eb6d3d40037222ce40f21e170a4400516
tokizuoh <tokizuoh@gmail.com>
2024-11-27 16:38:07 +0900

    feat: fetch query

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -19,4 +23,46 @@ func main() {
 		return
 	}
 	query := string(queryBytes)
+
+	// TODO: DI
+	variables := map[string]interface{}{
+		"owner":    "swiftlang",
+		"name":     "swift",
+		"first":    5,
+		"filePath": "CHANGELOG.md",
+	}
+
+	body := map[string]interface{}{
+		"query":     query,
+		"variables": variables,
+	}
+
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		fmt.Printf("Failed to marshal body: %v\n", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		fmt.Printf("Failed to create request: %v\n", err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Failed to send request: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Failed to read response: %v\n", err)
+		return
+	}
 }
